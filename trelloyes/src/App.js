9eb2d3113b81577e3e69f1b04f52d5f6ef912667
Caleb King <caleb.king536@gmail.com>
2019-11-11 14:27:45 -0700

    Updated List and Index.js

--- trelloyes/src/App.js
+++ trelloyes/src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 import List from './List.js';
 
 function App(props) {
-  console.log(props);
+  const lists = props.store.STORE.list.map(list => {
+    <List 
+      id={list.id}
+      header={list.header}
+      cardIds={list.cardIds}
+    />
+  });
+  
   return (
     <main className='App'>
       <header className="App-header">
         <h1>Trelloyes!</h1>
       </header>
-    <div className="App-list">
-      {props.store.STORE.lists.map(list => <List 
-      id={list.id}
-      header={list.header}
-      cardIds={list.cardIds}
-      />)}
-    </div>
+      <div className="App-list">
+        {lists}
+      </div>
     </main>
   );
 }
