48b582de2f59d668dbe58829d82469aa5f7cf103
Mason Reichbauer <mreichba@mail.usf.edu>
2019-11-11 15:06:59 -0500

    added to app.js

--- trelloyes/src/App.js
+++ trelloyes/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-function App() {
+function App(props) {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <header className="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+    <div className="App-list">
+
+    </div>
     </main>
   );
 }
