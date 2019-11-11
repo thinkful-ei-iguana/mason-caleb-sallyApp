b47f9d07a6bb05e936b41f7abcc303a5ad109cc1
Mason Reichbauer <mreichba@mail.usf.edu>
2019-11-11 15:55:18 -0500

    modded app.js

--- trelloyes/src/App.js
+++ trelloyes/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import store from './store.js';
 import List from './List.js';
 
 function App(props) {
+  console.log(props);
   return (
     <main className='App'>
       <header className="App-header">
         <h1>Trelloyes!</h1>
       </header>
     <div className="App-list">
-      {store.list.map(list => <List 
+      {props.store.STORE.lists.map(list => <List 
       id={list.id}
       header={list.header}
       cardIds={list.cardIds}
